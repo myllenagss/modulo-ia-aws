5520521e727cad07f062e236e620b4db4e8bea63
Myllena Silva <myllenasantossilva@gmail.com>
2025-11-16 19:49:11 -0300

    terceiro_comit

---
+++ atividade-01/calculadora_preco_total.py
@@ -0,0 +1,13 @@
+# Informações do produto
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+# Cálculo do preço total
+preco_total = preco_unitario * quantidade
+
+# Exibição das informações
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
